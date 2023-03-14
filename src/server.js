5c8aa8f1ce5e8ac4339926bb938c67ae3e1a2f65
ThiagoMorato <thiagosmorato@gmail.com>
2023-03-14 15:31:55 -0300

    Finished third module 'Streams no Node.js'

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from "node:http";
+import { json } from "./middlewares/json.js";
 
 // Principais métodos HTTP - GET / POST / PUT / PATCH / DELETE
 
@@ -10,24 +11,28 @@ import http from "node:http";
 
 const users = [];
 
-const server = http.createServer((request, response) => {
-  const { method, url } = request;
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+
+  await json(req, res);
 
   if (method === "GET" && url === "/users") {
-    return response.setHeader("Content-type", "application/json").end(JSON.stringify(users));
+    return res.end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
+
     users.push({
       id: 1,
-      name: "John Doe",
-      email: "johndoe@example.com",
+      name,
+      email,
     });
 
-    return response.writeHead(201).end();
+    return res.writeHead(201).end();
   }
 
-  return response.writeHead(404).end();
+  return res.writeHead(404).end();
 });
 
 //localhost:3333
